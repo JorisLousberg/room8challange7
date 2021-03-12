7a4b3c1321ce86deadfde8100caff1bb1dfe2ab8
galactorsus( <504972@vistacollege.nl>
2021-03-12 13:46:10 +0100

    basic layout update 1

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/mainstyle.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="pagecon">
+        <nav><div class="navbar"></div></nav>        
+        <div class="mainbody"></div>
+        <footer><div class="footerbar"></div></footer>        
+    </div>
+    
+</body>
+</html>

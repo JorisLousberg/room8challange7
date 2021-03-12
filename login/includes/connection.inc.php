6279714305e3e7ea3c00a24c90bea0ae5b23fe0f
C3c02 <505015@vistacollege.nl>
2021-03-12 13:44:53 +0100

    Added DB connection (login)

---
+++ login/includes/connection.inc.php
@@ -0,0 +1,17 @@
+<?php
+function connectDB() {
+    $dbhost = "localhost:3306";
+    $dbusername = "Ced02";
+    $dbpass = "_9D5sf3i";
+    $dbname = "db_vistashowroom";
+
+    try {
+        $connect = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpass);
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $error) {
+        $message = $error->getMessage();
+    }
+}
+
+connectDB();
+?>

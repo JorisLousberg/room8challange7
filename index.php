f969e21c60be3010311dff968977bb43702f47e4
CraZzyIceeNicK <405965@vistacollege.nl>
2021-03-12 13:51:15 +0100

    Footer Edit

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/mainstyle.css">
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <title>V!st@Cars - Home</title>
 </head>
 <body>
     <div class="pagecon">

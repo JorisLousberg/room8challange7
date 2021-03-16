9bc9d3b79b90897e1c7697f7513c8c2756fde59e
CraZzyIceeNicK <405965@vistacollege.nl>
2021-03-16 18:32:17 +0100

    border slideshow weg

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/mainstyle.css">
-    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <title>V!st@Cars - Home</title>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
@@ -45,7 +44,7 @@
                     </div>
                     <div class="card card-2">
                         <img src="images/Toyotachr.jpg">
-                        <div>Toyota Chr</div>
+                        <div>Toyota CHR</div>
                     </div>
                     <div class="card card-3">
                         <img src="images/reclame.jpg">

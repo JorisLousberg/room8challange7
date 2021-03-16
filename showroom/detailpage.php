81c5856b3abac096384fe6a405c83619fff551c1
C3c02 <505015@vistacollege.nl>
2021-03-16 11:54:37 +0100

    clean code + detailpage

---
+++ showroom/detailpage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/mainstyle.css">
+    <link rel="stylesheet" href="../css/detail.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <title>V!st@Cars - Showroom</title>
+</head>
+<body>
+<div class="pagecon">
+<nav>
+    <section class="navContainer">
+            <div class="logoWrapper"> <a href="../index.php"><img src="../images/vistacars.V3.png" alt="V!ist@ Cars" style="width:12vh;height:11vh;"></a> </div>
+        <nav class="navMenu">
+            <ul class="navList">
+                <li class="navItem"><a class="navLink" href="../index.php">Home</a></li>     
+            </ul>
+            <ul class="navList">
+                <li class="navItem"><a class="navLink" href="../showroom/">Showroom</a></li>     
+            </ul>
+            <ul class="navList">
+                <li class="navItem"><a class="navLink" href="contact.php">Contact</a></li>     
+            </ul>
+        </nav>
+    </section>
+</nav>
+
+<div id="detailcontent">
+    <img>
+</div>
+
+    <footer>
+          <div class="footer">
+            <div id="footercrtext">&copy; V!st@Cars(2021)</div>
+            <div id="footerteltext">Telefoonnummer: 06 12345678</div>
+            <a href="login/login.php" id="loginbtn">Inloggen</a>
+          </div>
+      </footer>        
+    </div>
+</div>
+</body>
+</html>
